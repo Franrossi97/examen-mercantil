23834a170a36be31918a27e79f8134259a995f09
Franco Rossi <franrossi97@gmail.com>
2022-10-27 23:48:17 -0300

    TestFolder

---
+++ src/test/java/examen/mercantil/examenmercantil/ExamenMercantilApplicationTests.java
@@ -0,0 +1,13 @@
+package examen.mercantil.examenmercantil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenMercantilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
