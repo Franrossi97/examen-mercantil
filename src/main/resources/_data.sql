9313cdfaade67fde9b5ff4e8e608b387b8e1ee89
Franco Rossi <franrossi97@gmail.com>
2022-10-27 23:48:44 -0300

    Application properties

---
+++ src/main/resources/_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO PRODUCTOS ( DESCRIPCION_CORTA , DESCRIPCION_LARGA , NOMBRE , PRECIO_UNITARIO ) VALUES
+("Rica pizza", "Muzzarella", "Pizza", 2000),
+("Rica pizza", "Fugazzeta", "Pizza", 2500),
+("Rica pizza", "Americana", "Pizza", 3000);
