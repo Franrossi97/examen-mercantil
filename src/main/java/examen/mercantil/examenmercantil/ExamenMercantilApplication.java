4e27f82a6968b1d5d8a24997bc19291a86eba680
Franco Rossi <franrossi97@gmail.com>
2022-10-27 23:48:07 -0300

    Main

---
+++ src/main/java/examen/mercantil/examenmercantil/ExamenMercantilApplication.java
@@ -0,0 +1,13 @@
+package examen.mercantil.examenmercantil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenMercantilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenMercantilApplication.class, args);
+	}
+
+}
