529923367b5aa96987d4b955acdc4498a8bcb750
Franco Rossi <franrossi97@gmail.com>
2022-10-29 18:17:59 -0300

    Cambios en exception

--- src/main/java/examen/mercantil/examenmercantil/services/ProductService.java
+++ src/main/java/examen/mercantil/examenmercantil/services/ProductService.java
@@ -2,15 +2,15 @@ package examen.mercantil.examenmercantil.services;
 
 import examen.mercantil.examenmercantil.model.dtos.ProductDto;
 import examen.mercantil.examenmercantil.model.entities.Product;
-import examen.mercantil.examenmercantil.model.exceptions.ProductNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public interface ProductService {
 
     Product createProduct(ProductDto product);
 
-    Product findProduct(String productId) throws ProductNotFoundException;
+    Product findProduct(String productId) throws ResponseStatusException;
 
     Product editProduct(String productId, ProductDto product);
 
